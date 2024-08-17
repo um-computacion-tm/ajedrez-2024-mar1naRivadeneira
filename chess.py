c877166efc76ae2ee991e3233b6b1306429a61fc
mar1naRivadeneira <m.rivadeneira@um.edu.ar>
2024-08-16 23:39:11 -0300

    validacion de coordenadas

--- chess.py
+++ chess.py
@@ -6,10 +6,17 @@ class Chess:
         self.__turn__ = "WHITE"
 
     def move( self, from_row, from_col, to_row, to_col, ):
-      # validate coords
+        #validacion de coordenadas
+        if not self.__is_valid_position(from_row, from_col):
+            raise ValueError("posicion de origen invalida")
+        if not self.__is_valid_position(to_row, to_col):
+            raise ValueError("posicion de destino invalida")
+        
         piece = self.__board__.get_piece(from_row, from_col)
         self.change_turn()
     
+    def __is_valid_position(self, row, col):
+        return 0 <= row < 8 and 0 <= col < 8
     
     def change_turn(self):
         if self.__turn__ == "WHITE":
