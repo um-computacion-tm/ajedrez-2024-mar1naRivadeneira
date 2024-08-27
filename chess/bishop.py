ab40fa0dd97d7a031ada14a462fb85486cc78335
mar1naRivadeneira <m.rivadeneira@um.edu.ar>
2024-08-27 09:31:30 -0300

    arreglo de las piezas

--- chess/bishop.py
+++ chess/bishop.py
@@ -6,3 +6,8 @@ class Bishop(Piece):
    def mover(self):
       pass
 
+   def __str__(self):  #devuelve el simbolo de la torre segun el color de la pieza
+      if self.__color__ == "WHITE":
+          return "♗"
+      else:
+         return "♝" 
