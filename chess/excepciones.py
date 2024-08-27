de6fde0c62a903c50c8ec3a5a3006f698514e954
mar1naRivadeneira <m.rivadeneira@um.edu.ar>
2024-08-27 10:02:59 -0300

    arreglo en excepciones

--- chess/excepciones.py
+++ chess/excepciones.py
@@ -1,5 +1,15 @@
 
 #jeraquia de invalidmove de rook,piece,pawn pero todas son movimientos invalidos pero algunas especificos
+class InvalidMove(Exception):  # Funciones para manejar excepciones estándar
+    pass
+
+class InvalidMoveNoPiece(InvalidMove):  #Excepción para un movimiento desde una casilla vacía.
+    ...
+
+class InvalidMoveRookMove(InvalidMove): #Excepción para un movimiento inválido de la torre.
+    ...
+
+
 
 
 #guardado en raise es opcional no es un requisito obligatorio
