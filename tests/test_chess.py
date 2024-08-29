5df89f4714a7668ce8d5d9a3d37899775c637d3d
mar1naRivadeneira <m.rivadeneira@um.edu.ar>
2024-08-28 23:11:48 -0300

    test de cambio de turno

--- tests/test_chess.py
+++ tests/test_chess.py
@@ -6,11 +6,17 @@ from chess.chess import Chess
 class TestChess(unittest.TestCase):
     
     def setUp(self):
-        self.__game__ = Chess()
+        self.chess = Chess()
         
-    def test_initial_turn(self):
-        self.assertEqual(self.__game__.__turn__, "WHITE" , "El turno debe ser de las blancas")
-
+    def test_change_turn(self): 
+        #test para verificar que el turno cambia de  blanco a negro
+        self.assertEqual(self.chess.turn, "WHITE")
+        self.chess.change_turn()
+        self.assertEqual(self.chess.turn, "BLACK")
+        
+        #test pars verificar que el turno cambia de negro a blanco
+        self.chess.change_turn()
+        self.assertEqual(self.chess.turn, "WHITE")
         
 if __name__ == '__main__':
     unittest.main()                
