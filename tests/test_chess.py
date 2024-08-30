95448e07b43203eebb8757d76cf9f2cfb4a6a2c7
mar1naRivadeneira <m.rivadeneira@um.edu.ar>
2024-08-29 22:41:21 -0300

    test para verificar el cambio de turno despues de un movimiento

--- tests/test_chess.py
+++ tests/test_chess.py
@@ -8,7 +8,7 @@ class TestChess(unittest.TestCase):
     def setUp(self):
         self.chess = Chess()
         
-    def test_change_turn(self): 
+    def test_change_turn(self): #comprueba el cambio de turno entre jugadores 
         #test para verificar que el turno cambia de  blanco a negro
         self.assertEqual(self.chess.turn, "WHITE")
         self.chess.change_turn()
@@ -17,6 +17,19 @@ class TestChess(unittest.TestCase):
         #test pars verificar que el turno cambia de negro a blanco
         self.chess.change_turn()
         self.assertEqual(self.chess.turn, "WHITE")
+        
+    def test_move_changes_turn(self):#verifica el cambio de turno despues de un movimiento
+        #comprobar el turno incial
+        turno_inicial = self.chess.turn
+        self.assertEqual(turno_inicial, "WHITE")
+        
+        #realizo un movimiento
+        self.chess.move(0, 0, 1, 0)
+        
+        #comprueba que el turno cambio
+        new_turn = self.chess.turn
+        self.assertNotEqual(new_turn, turno_inicial)    
+        self.assertEqual(new_turn, "BLACK")
         
 if __name__ == '__main__':
     unittest.main()                
