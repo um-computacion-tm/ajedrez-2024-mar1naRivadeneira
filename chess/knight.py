31573c11c6ddc5e064093d826c3bc243d1251016
mar1naRivadeneira <m.rivadeneira@um.edu.ar>
2024-09-01 22:26:47 -0300

    arreglo en el color de la pieza

--- chess/knight.py
+++ chess/knight.py
@@ -1,12 +1,11 @@
 #se mueve en forma de L y puede saltearse otras piezas
 from chess.piece import Piece
 
-class Knight(Piece):
+class Knight(Piece): 
+   white_str = "♞"  
+   black_str = "♘"
+   
    def mover(self):
       pass
 
-   def __str__(self):  #devuelve el simbolo de la torre segun el color de la pieza
-      if self.__color__ == "WHITE":
-          return "♘"
-      else:
-         return "♞" 
+   
