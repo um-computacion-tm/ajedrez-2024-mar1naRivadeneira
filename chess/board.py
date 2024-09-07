ba8572c52d70666498d0b45d8a50cc1971d81972
mar1naRivadeneira <m.rivadeneira@um.edu.ar>
2024-09-06 21:59:48 -0300

    pequeño arreglo

--- chess/board.py
+++ chess/board.py
@@ -5,6 +5,7 @@ from chess.king import King
 from chess.bishop import Bishop
 from chess.knight import Knight
 from chess.excepciones import OutOfBoard
+
 class Board:
     def __init__(self, for_test = False): #para que en los test de cada pieza no aparezcan las demas
         self.__positions__ = []
@@ -13,30 +14,31 @@ class Board:
             for _ in range(8):
                 col.append(None)
             self.__positions__.append(col)
-        self.__positions__[0][0] = Rook("BLACK", self) # Black
-        self.__positions__[0][7] = Rook("BLACK", self) # Black
-        self.__positions__[7][7] = Rook("WHITE", self) # White
-        self.__positions__[7][0] = Rook("WHITE", self) # White
+        if not for_test:    
+                self.__positions__[0][0] = Rook("BLACK", self) # Black
+                self.__positions__[0][7] = Rook("BLACK", self) # Black
+                self.__positions__[7][7] = Rook("WHITE", self) # White
+                self.__positions__[7][0] = Rook("WHITE", self) # White
+                
+                self.__positions__[0][1] = Knight("BLACK", self) # Black
+                self.__positions__[0][6] = Knight("BLACK", self) # Black
+                self.__positions__[7][1] = Knight("WHITE", self) # White
+                self.__positions__[7][6] = Knight("WHITE", self) # White
+                
+                self.__positions__[0][2] = Bishop("BLACK", self) # Black
+                self.__positions__[0][5] = Bishop("BLACK", self) # Black
+                self.__positions__[7][2] = Bishop("WHITE", self) # White
+                self.__positions__[7][5] = Bishop("WHITE", self) # White
+                
+                self.__positions__[0][3] = Queen("BLACK", self) # Black
+                self.__positions__[7][3] = Queen("WHITE", self) # White
+                
+                self.__positions__[0][4] = King("BLACK", self) # Black
+                self.__positions__[7][4] = King("WHITE", self) # White
         
-        self.__positions__[0][1] = Knight("BLACK", self) # Black
-        self.__positions__[0][6] = Knight("BLACK", self) # Black
-        self.__positions__[7][1] = Knight("WHITE", self) # White
-        self.__positions__[7][6] = Knight("WHITE", self) # White
-        
-        self.__positions__[0][2] = Bishop("BLACK", self) # Black
-        self.__positions__[0][5] = Bishop("BLACK", self) # Black
-        self.__positions__[7][2] = Bishop("WHITE", self) # White
-        self.__positions__[7][5] = Bishop("WHITE", self) # White
-        
-        self.__positions__[0][3] = Queen("BLACK", self) # Black
-        self.__positions__[7][3] = Queen("WHITE", self) # White
-        
-        self.__positions__[0][4] = King("BLACK", self) # Black
-        self.__positions__[7][4] = King("WHITE", self) # White
-        
-        for col in range(8):
-            self.__positions__[1][col]= Pawn("BLACK", self)
-            self.__positions__[6][col]= Pawn("WHITE", self)
+                for col in range(8):
+                    self.__positions__[1][col]= Pawn("BLACK", self)
+                    self.__positions__[6][col]= Pawn("WHITE", self)
             
     def __str__(self): 
         board_str = ""
