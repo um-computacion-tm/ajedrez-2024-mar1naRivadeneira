384741cee0a49ee930bda636f6e9f1ace3f238ae
mar1naRivadeneira <m.rivadeneira@um.edu.ar>
2024-09-06 22:13:06 -0300

    ...

--- tests/test_pieces.py
+++ tests/test_pieces.py
@@ -4,17 +4,6 @@ from chess.piece import Piece
 class TestPiece(unittest.TestCase):
     def setUp(self):
         self.piece = Piece('WHITE', None)
-        
-    def test_mover(self):
-        try:
-            self.piece.mover()
-            print("mover fue llamado bcon exito")
-        except Exception as e:
-            self.fail(f"mover() raised an exception: {e}")        
-        
-    
-    
-    
-    
+       
 if __name__ == '__main__':
     unittest.main()       
