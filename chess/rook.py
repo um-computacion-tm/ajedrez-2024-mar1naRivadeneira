af7c19cfa2a8e71092b14c09e74a0f123d104792
mar1naRivadeneira <m.rivadeneira@um.edu.ar>
2024-09-06 22:09:49 -0300

    ...

--- chess/rook.py
+++ chess/rook.py
@@ -36,11 +36,3 @@ class Rook(Piece):
         for next_row in range(row - 1, -1, -1):
             possibles.append((next_row, col))
         return possibles
-
-   '''def is_valid_move(self, from_row, from_col, to_row, to_col, board):
-      if from_col == to_col:
-         step = 1 if to_row > from_row else -1   
-         for row in range(from_col + step, to_col, step):
-            if board.get_piece(row, from_col ) is not None:
-               return False
-         return True  ''' 
