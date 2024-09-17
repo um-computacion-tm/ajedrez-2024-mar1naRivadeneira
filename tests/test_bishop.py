7b1f068180562528b9f069e59ad778fa8aec7da3
mar1naRivadeneira <m.rivadeneira@um.edu.ar>
2024-09-17 14:05:22 -0300

    agregando test movimientos diagonales asc y desc

--- tests/test_bishop.py
+++ tests/test_bishop.py
@@ -3,6 +3,8 @@ from chess.bishop import Bishop
 from chess.board import Board
 
 class TestBishop(unittest.TestCase):
+    def setUp(self):
+        self.board = Board(for_test = True)
     
     def test_str(self):
         board = Board()
@@ -12,14 +14,38 @@ class TestBishop(unittest.TestCase):
             "♗",
         )
     
-    def test_move_up_izquierda(self):
-        board = Board(for_test = True)
-        bishop = Bishop("WHITE", board)
+    def test_move_diagonal_asc_izquierda(self):
+        bishop = Bishop("WHITE", self.board)
         possibles = bishop.possible_positions_dai(3, 3)
         self.assertEqual(
             possibles,
             [(2, 2), (1, 1), (0, 0)]
         )
+     
+    def test_move_diagonal_asc_derecha(self):
+        bishop = Bishop("WHITE", self.board)
+        possibles = bishop.possible_positions_dad(4, 4)
+        self.assertEqual(
+            possibles,
+            [(3, 5), (2, 6), (1, 7)]    
+        )
+        
+    def test_move_diagonal_desc_izquierda(self):
+        bishop = Bishop("WHITE", self.board)
+        possibles = bishop.possible_positions_ddi(4, 4)
+        self.assertEqual(
+            possibles,
+            [(5, 3), (6, 2), (7, 1)]
+        )
+        
+    def test_move_diagonal_desc_derecha(self):
+        bishop = Bishop("WHITE", self.board)
+        possibles = bishop.possible_positions_ddd(4, 4)
+        self.assertEqual(
+            possibles,
+            [(5, 5), (6, 6), (7, 7)]
+        )
+        
     
 if __name__ == '__main__':
     unittest.main()       
