d89bbbf6f1785ec162592c33136dfe0bb381b8f1
mar1naRivadeneira <m.rivadeneira@um.edu.ar>
2024-09-26 20:39:42 -0300

    cambio en pawn para arreglo de issue en codeclimate

--- chess/pawn.py
+++ chess/pawn.py
@@ -1,13 +1,45 @@
-#movimiento inicial de dos casillas, captura en diagonal
+#movimiento inicial de dos casillas, captura en diagonal, al llegar a la ultima fila se vuelve una reina "promote"
 from chess.piece import Piece
+from chess.queen import Queen
+
 
 class Pawn(Piece):
      white_str = "♙" 
      black_str = "♟"
    
-#PROMOTE: cuando el peon llega al final del tablero se convierte en una reina/rey y se valida en el peon, no en el board o chess
+     def possible_moves(self, row, col):
+          directions = [(1, 0)]  
 
-#movimiento del peon
+          # direcciones según el color del peón
+          if self.__color__ == "WHITE":
+               directions = [(-1, 0)]  # movimiento hacia adelante blanco
+               capture_directions = [(-1, 1), (-1, -1)]  #captura en diagonal
+          else:
+               directions = [(1, 0)]  # movimiento hacia adelante negro
+               capture_directions = [(1, 1), (1, -1)] 
+               
+          #movimientos generales hacia adelante sin captura
+          moves = self.general_moves(row, col, directions, single_step=True)
+          
+          #cappturas diagonales
+          for dir_row, dir_col in capture_directions:
+               next_row, next_col = row + dir_row, col + dir_col
+               if 0 <= next_row < 8 and 0 <= next_col < 8 and self.can_eat(next_row, next_col):
+                    moves.append((next_row, next_col))
+          
+          #movimiento doble desde la fila inicial
+          if self.__color__ == "WHITE" and row == 6 and not self.is_occupied(row - 1, col):
+               if not self.is_occupied(row - 2, col):
+                    moves.append((row - 2, col))
+          elif self.__color__ == "BLACK" and row == 1 and not self.is_occupied(row + 1, col):
+               if not self.is_occupied(row + 2, col):
+                    moves.append((row + 2, col))
+
+          return moves
+
+    
+
+'''#movimiento del peon
      def get_possible_positions(self, from_row, from_col):
         possibles = self.get_possible_positions_move(
             from_row,
@@ -73,4 +105,4 @@ class Pawn(Piece):
                          ]
                     else:
                          return []
-          return []
+          return []'''
