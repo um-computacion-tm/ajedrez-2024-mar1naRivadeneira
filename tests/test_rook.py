7358277dfd4b7c5b9f025092694e5c37faecbc1d
mar1naRivadeneira <m.rivadeneira@um.edu.ar>
2024-10-03 21:33:09 -0300

    arreglo en el nombre de la funcion

--- tests/test_rook.py
+++ tests/test_rook.py
@@ -16,13 +16,13 @@ class TestRook(unittest.TestCase):
     def test_move_vertical_desc(self):
         board = Board()
         rook = Rook("WHITE", board)
-        possibles = rook.valid_positions(4, 1, 5, 1)
+        possibles = rook.get_valid_moves(4, 1, 5, 1)
         self.assertTrue(possibles)
            
     def test_move_vertical_asc(self):
         board = Board()
         rook = Rook("WHITE", board)
-        possibles = rook.valid_positions(4, 1, 3, 1)
+        possibles = rook.get_valid_moves(4, 1, 3, 1)
         self.assertTrue(possibles)
 
     def test_move_vertical_desc_with_own_piece(self):
@@ -30,7 +30,7 @@ class TestRook(unittest.TestCase):
         board.set_piece(6, 1, Pawn("WHITE", board))
         rook = Rook("WHITE", board)
         board.set_piece(4, 1, rook)
-        possibles = rook.valid_positions(4, 1, 5, 1)
+        possibles = rook.get_valid_moves(4, 1, 5, 1)
         self.assertTrue(possibles)
 
     def test_move_vertical_desc_with_not_own_piece(self):
@@ -38,19 +38,19 @@ class TestRook(unittest.TestCase):
         board.set_piece(6, 1, Pawn("BLACK", board))
         rook = Rook("WHITE", board)
         board.set_piece(4, 1, rook)
-        possibles = rook.valid_positions(4, 1, 6, 1)
+        possibles = rook.get_valid_moves(4, 1, 6, 1)
         self.assertTrue(possibles)
         
     def test_move_horizontal_derecha(self):
         board = Board(for_test=True)
         rook = Rook("WHITE", board)  
-        possibles = rook.valid_positions(5, 1, 5, 3)
+        possibles = rook.get_valid_moves(5, 1, 5, 3)
         self.assertTrue(possibles)
            
     def test_move_horizontal_izquierda(self):
         board = Board(for_test=True)
         rook = Rook("WHITE", board)  
-        possibles = rook.valid_positions(5, 7, 5, 5)
+        possibles = rook.get_valid_moves(5, 7, 5, 5)
         self.assertTrue(possibles)
            
     def test_move_horizontal_derecha_with_own_piece(self):
@@ -58,7 +58,7 @@ class TestRook(unittest.TestCase):
         board.set_piece(5, 5, Pawn("WHITE", board))
         rook = Rook("WHITE", board)
         board.set_piece(5, 1, rook)
-        possibles = rook.valid_positions(5, 1, 5, 3)
+        possibles = rook.get_valid_moves(5, 1, 5, 3)
         self.assertTrue(possibles)
         
     def test_move_horizontal_derecha_with_not_own_piece(self):
@@ -66,7 +66,7 @@ class TestRook(unittest.TestCase):
         board.set_piece(5, 5, Pawn("BLACK", board))
         rook = Rook("WHITE", board)
         board.set_piece(5, 1, rook)
-        possibles = rook.valid_positions(5, 1, 5, 5)
+        possibles = rook.get_valid_moves(5, 1, 5, 5)
         self.assertTrue(possibles)
         
     def test_move_horizontal_izquierda_with_own_piece(self):
@@ -74,7 +74,7 @@ class TestRook(unittest.TestCase):
         board.set_piece(5, 3, Pawn("WHITE", board))
         rook = Rook("WHITE", board)
         board.set_piece(5, 7, rook)  
-        possibles = rook.valid_positions(5, 7, 5, 4)
+        possibles = rook.get_valid_moves(5, 7, 5, 4)
         self.assertTrue(possibles)
         
     def test_move_horizontal_izquierda_with_not_own_piece(self):
@@ -82,13 +82,13 @@ class TestRook(unittest.TestCase):
         board.set_piece(5, 3, Pawn("BLACK", board))
         rook = Rook("WHITE", board)
         board.set_piece(5, 7, rook)     
-        possibles = rook.valid_positions(5, 7, 5, 3)
+        possibles = rook.get_valid_moves(5, 7, 5, 3)
         self.assertTrue(possibles)
         
     def test_move_diagonal_desc(self):
         board = Board()
         rook = board.get_piece(col=0, row=0)
-        is_possible = rook.valid_positions(
+        is_possible = rook.get_valid_moves(
             from_row=0,
             from_col=0,
             to_row=1,
