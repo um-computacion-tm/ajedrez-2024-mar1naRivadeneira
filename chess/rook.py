def5305c3a96671fafde2c64599cef30db022286
mar1naRivadeneira <m.rivadeneira@um.edu.ar>
2024-10-03 21:29:23 -0300

    cambio en el nombre de la funcion

--- chess/rook.py
+++ chess/rook.py
@@ -5,7 +5,7 @@ class Rook(Piece):
    white_str = "♖"  
    black_str = "♜"    
    
-   def valid_positions(self, from_row, from_col, to_row, to_col):
+   def get_valid_moves(self, from_row, from_col, to_row, to_col):
      # Movimientos ortogonales para la torre
      possible_move= self.general_moves(from_row, from_col, self.__rook_directions__, single_step=False)
      return (to_row, to_col)in possible_move
