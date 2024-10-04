174720fd1f113a152f26522599114cc20176a174
mar1naRivadeneira <m.rivadeneira@um.edu.ar>
2024-10-03 21:30:16 -0300

    cambio en el nombre

--- tests/test_bishop.py
+++ tests/test_bishop.py
@@ -16,64 +16,64 @@ class TestBishop(unittest.TestCase):
     
     def test_move_diagonal_asc_izquierda(self):
         bishop = Bishop("WHITE", self.board)
-        possibles = bishop.valid_positions(3, 3, 0, 0)
+        possibles = bishop.get_valid_moves(3, 3, 0, 0)
         self.assertTrue(possibles)
      
     def test_move_diagonal_asc_derecha(self):
         bishop = Bishop("WHITE", self.board)
-        possibles = bishop.valid_positions(4, 4, 2, 6)
+        possibles = bishop.get_valid_moves(4, 4, 2, 6)
         self.assertTrue(possibles)
         
     def test_move_diagonal_desc_izquierda(self):
         bishop = Bishop("WHITE", self.board)
-        possibles = bishop.valid_positions(4, 4, 7, 1)
+        possibles = bishop.get_valid_moves(4, 4, 7, 1)
         self.assertTrue(possibles)
         
     def test_move_diagonal_desc_derecha(self):
         bishop = Bishop("WHITE", self.board)
-        possibles = bishop.valid_positions(4, 4, 6, 6)
+        possibles = bishop.get_valid_moves(4, 4, 6, 6)
         self.assertTrue( possibles,)
         
     def test_move_diagonal_asc_izquierda_with_own_piece(self):
         self.board.set_piece(2, 2, Bishop("WHITE", self.board))
         bishop = Bishop("WHITE", self.board)
         self.board.set_piece(5, 5, bishop)
-        possibles = bishop.valid_positions(5, 5, 3, 3)
+        possibles = bishop.get_valid_moves(5, 5, 3, 3)
         self.assertTrue(possibles) 
         
     def test_move_diagonal_asc_izquierda_with_opponent_piece(self):
         self.board.set_piece(3, 3, Bishop("BLACK", self.board))
         bishop = Bishop("WHITE", self.board)
         self.board.set_piece(4, 4, bishop)
-        possibles = bishop.valid_positions(4, 4, 3, 3)
+        possibles = bishop.get_valid_moves(4, 4, 3, 3)
         self.assertTrue(possibles)               
      
     def test_move_diagonal_asc_derecha_with_own_piece(self):
         self.board.set_piece(3, 5, Bishop("WHITE", self.board))
         bishop = Bishop("WHITE", self.board)
         self.board.set_piece(4, 4, bishop)
-        possibles = bishop.valid_positions(4, 4, 5, 5)                      
+        possibles = bishop.get_valid_moves(4, 4, 5, 5)                      
         self.assertTrue(possibles)
         
     def test_move_diagonal_asc_derecha_with_opponent_piece(self):
         self.board.set_piece(3, 5, Bishop("BLACK", self.board))
         bishop = Bishop("WHITE", self.board)
         self.board.set_piece(4, 4, bishop)
-        possibles = bishop.valid_positions(4, 4, 3, 5)
+        possibles = bishop.get_valid_moves(4, 4, 3, 5)
         self.assertTrue(possibles)  
         
     def test_move_diagonal_desc_izquierda_with_own_piece(self):
         self.board.set_piece(6, 2, Bishop("WHITE", self.board))
         bishop = Bishop("WHITE", self.board)
         self.board.set_piece(4, 4, bishop)
-        possibles = bishop.valid_positions(4, 4, 5, 3)
+        possibles = bishop.get_valid_moves(4, 4, 5, 3)
         self.assertTrue(possibles)
         
     def test_move_diagonal_desc_derecha_with_opponent_piece(self):
         self.board.set_piece(6, 6, Bishop("BLACK", self.board))
         bishop = Bishop("WHITE", self.board)
         self.board.set_piece(4, 4, bishop)
-        possibles = bishop.valid_positions(4, 4, 6, 6)
+        possibles = bishop.get_valid_moves(4, 4, 6, 6)
         self.assertTrue(possibles)
           
 if __name__ == '__main__':
