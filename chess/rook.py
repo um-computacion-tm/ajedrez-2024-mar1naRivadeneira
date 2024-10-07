ed983ef3f4ea1e3fec065903506654389451385f
mar1naRivadeneira <m.rivadeneira@um.edu.ar>
2024-10-07 19:09:00 -0300

    arreglo para evitar  issue

--- chess/rook.py
+++ chess/rook.py
@@ -2,10 +2,13 @@
 from chess.piece import Piece   
 
 class Rook(Piece):
-   white_str = "♖"  
-   black_str = "♜"    
+  white_str = "♖"  
+  black_str = "♜"    
    
-   def get_valid_moves(self, from_row, from_col, to_row, to_col):
+  def get_valid_moves(self, from_row, from_col, to_row, to_col):
+      return self.is_valid_rook_move(from_row, from_col, to_row, to_col)
+    
+  def is_valid_rook_move(self, from_row, from_col, to_row, to_col): 
      # Movimientos ortogonales para la torre
      possible_move= self.general_moves(from_row, from_col, self.__rook_directions__, single_step=False)
      return (to_row, to_col)in possible_move
