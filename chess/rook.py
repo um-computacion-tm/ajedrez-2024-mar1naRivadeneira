30f5482536cb653b84e8f9292577ec5b48d59e13
mar1naRivadeneira <m.rivadeneira@um.edu.ar>
2024-10-09 21:44:20 -0300

    arreglo para evitar duplicacion de codigo

--- chess/rook.py
+++ chess/rook.py
@@ -6,9 +6,11 @@ class Rook(Piece):
   black_str = "♜"    
    
   def get_valid_moves(self, from_row, from_col, to_row, to_col):
-      return self.is_valid_rook_move(from_row, from_col, to_row, to_col)
+      directions = self.is_valid_rook_move()
+      possible_moves = self.general_moves(from_row, from_col, directions, single_step=False )
+      return (to_row, to_col) in possible_moves
+
     
-  def is_valid_rook_move(self, from_row, from_col, to_row, to_col): 
+  def is_valid_rook_move(self): 
      # Movimientos ortogonales para la torre
-     possible_move= self.general_moves(from_row, from_col, self.__rook_directions__, single_step=False)
-     return (to_row, to_col)in possible_move
+     return [(-1, 0), (1, 0), (0, -1), (0, 1)]
