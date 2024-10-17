20c2e82c1ea89a2cf27a625daa1357455ce5dac2
mar1naRivadeneira <m.rivadeneira@um.edu.ar>
2024-10-17 12:09:17 -0300

    agregando documentacion

--- chess/king.py
+++ chess/king.py
@@ -6,20 +6,18 @@ class King(Piece):
    black_str = "♚"
    
    def get_valid_moves(self, from_row, from_col, to_row, to_col):
-      """Comprueba si el movimiento hacia las coordenadas dadas es permitido para el Rey.
+      """
+        Comprueba si el movimiento hacia las coordenadas dadas es permitido para el Rey.
         Este método recibe las coordenadas actuales de el Rey y la casilla de destino. 
         El Rey puede moverse una casilla en cualquier dirección
-
-        Parámetros: 
-        - La posición actual del Rey (fila y columna).
-        - La posición de destino a la que se desea mover el Rey (fila y columna).
 
         Retorna:
         bool:
             Devuelve True si el movimiento es válido según las reglas para el Rey, 
             False en caso contrario.
                """
-      return (to_row, to_col) in self.calculate_king_moves(from_row, from_col)
+      possible_king_positions = self.calculate_king_moves(from_row, from_col)
+      return (to_row, to_col) in possible_king_positions
    
    def calculate_king_moves(self, from_row, from_col):
       """Calcula los movimientos válidos para el rey, limitados a un paso en cualquier dirección.
